3b25fb2f1d01e7d546a628314ba350686e2bcedb
Scorpi-ON <s.scorpi-on@ya.ru>
2025-03-25 19:22:46 +0300

    feat: added some configs

---
+++ src/conf/site-metadata.ts
@@ -0,0 +1,15 @@
+const siteMetadata = {
+    name: "KNTista",
+    short_name: "KNTista",
+    description: "Проект для автоматизации нужд профбюро ИКНТ",
+    repo_url: "https://github.com/code-418-dpr/KNTista",
+    authors: { name: "Код 418", url: "https://github.com/code-418-dpr" },
+    email: "s.scorpi-on@ya.ru",
+    app_icon: {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+    },
+};
+
+export default siteMetadata;
